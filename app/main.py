3bb13c6938bbdc7c19609cbdc5b0d56a4e276879
Hercules Aleixo <hercules.aleixo@selectsolucoes.com>
2025-09-27 20:09:21 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,8 +13,12 @@ def main():
     client_conn, client_addr = server_socket.accept()
     print(f"Connected by {client_addr}")
     data = client_conn.recv(1024).decode('utf-8')
-    match =  re.search("GET / ", data)
+    match =  re.search("GET /echo/", data)
     if match:
+        received_str = data.split("\r\n")[0].split("echo/")[1].split(" ")[0]
+        content_lenght = len(received_str)
+        client_conn.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_lenght}\r\n\r\n{received_str}".encode())
+    elif re.search("GET / ", data):
         client_conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     else:
         client_conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
