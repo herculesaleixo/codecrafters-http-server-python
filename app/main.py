edd0ef21ddd58da129bc1a9fd8c50d167672bf21
Hercules Aleixo <hercules.aleixo@selectsolucoes.com>
2025-09-27 19:50:54 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
+import re
 
 
 def main():
@@ -10,8 +11,13 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221))#=, reuse_port=True)
     client_conn, client_addr = server_socket.accept()
-    print(client_conn)
-    client_conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    print(f"Connected by {client_addr}")
+    data = client_conn.recv(1024).decode('utf-8')
+    match =  re.search("GET / ", data)
+    if match:
+        client_conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        client_conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
     main()
