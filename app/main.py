8c3af36d654e3d4bf0ed88086076623bb4871d89
Hercules Aleixo <herculesaleixo@MacBook-Pro-de-Hercules.local>
2025-09-28 13:35:01 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,11 +13,14 @@ def main():
     client_conn, client_addr = server_socket.accept()
     print(f"Connected by {client_addr}")
     data = client_conn.recv(1024).decode('utf-8')
-    match =  re.search("GET /echo/", data)
-    if match:
+    if re.search("GET /echo/", data):
         received_str = data.split("\r\n")[0].split("echo/")[1].split(" ")[0]
         content_lenght = len(received_str)
         client_conn.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_lenght}\r\n\r\n{received_str}".encode())
+    elif re.search("GET /user-agent", data):
+        user_agent = data.split("User-Agent: ")[1].split("\r\n")[0]
+        content_lenght = len(user_agent)
+        client_conn.sendall(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_lenght}\r\n\r\n{user_agent}".encode())
     elif re.search("GET / ", data):
         client_conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     else:
